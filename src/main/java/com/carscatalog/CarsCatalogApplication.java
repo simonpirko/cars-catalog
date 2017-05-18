27ec063a92e41acdaa9565da636e60594e00a88c
Vadzim Prudnikau <vadzim.prudnikau@leverx.com>
2017-05-18 18:22:56 +0300

    Task "Cars catalog": A project structure has been created with a minimum set.

---
+++ src/main/java/com/carscatalog/CarsCatalogApplication.java
@@ -0,0 +1,12 @@
+package com.carscatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarsCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarsCatalogApplication.class, args);
+	}
+}
