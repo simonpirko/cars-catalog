c2714289b4c007258a1ab94d0e4c10b08bef0f4f
Simon P <sipi7gm@gmail.com>
2017-06-07 04:16:52 +0300

    Task "Cars catalog": Added LoginController(implementation SpringSecurity), UserController(view user info); Added models: Role, User and them implementation; Added service: User; Added Registration impl. and sign in; Added role guest(not change auto, can crete new auto), user(change auto, create new auto); Added frontend: registration, user, login; Added upload image auto;

--- src/main/java/com/carscatalog/CarsCatalogApplication.java
+++ src/main/java/com/carscatalog/CarsCatalogApplication.java
@@ -1,23 +1,12 @@
 package com.carscatalog;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.transaction.PlatformTransactionManager;
-
-import javax.sql.DataSource;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class CarsCatalogApplication {
-
-	@Qualifier("dataSource")
-	@Autowired
-	private DataSource dataSource;
-
-	@Autowired
-	private PlatformTransactionManager transactionManager;
-
 	public static void main(String[] args) {
 		SpringApplication.run(CarsCatalogApplication.class, args);
 	}
